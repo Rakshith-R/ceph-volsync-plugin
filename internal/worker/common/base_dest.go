d0f9dad33aeea74eba0e067c9abf9f35a6f5c7da
Rakshith R <rar@redhat.com>
2026-03-19 20:42:15 +0530

    refactor: add BaseDestinationWorker
    Signed-off-by: Rakshith R <rar@redhat.com>

---
+++ internal/worker/common/base_dest.go
@@ -0,0 +1,48 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package common
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"google.golang.org/grpc"
+
+	apiv1 "github.com/RamenDR/ceph-volsync-plugin/internal/mover/proto/api/v1"
+)
+
+// BaseDestinationWorker provides shared destination
+// worker scaffolding.
+type BaseDestinationWorker struct {
+	Logger logr.Logger
+	Config DestinationConfig
+}
+
+// Run starts the gRPC destination server with
+// default MaxRecvMsgSize applied.
+func (w *BaseDestinationWorker) Run(
+	ctx context.Context,
+	dataServer apiv1.DataServiceServer,
+) error {
+	w.Logger.Info("Starting destination worker")
+
+	return RunDestinationServer(
+		ctx, w.Logger,
+		w.Config.ServerPort, dataServer,
+		grpc.MaxRecvMsgSize(MaxGRPCMessageSize),
+	)
+}
---
+++ internal/worker/common/base_dest_test.go
@@ -0,0 +1,67 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package common_test
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/go-logr/logr"
+
+	apiv1 "github.com/RamenDR/ceph-volsync-plugin/internal/mover/proto/api/v1"
+	"github.com/RamenDR/ceph-volsync-plugin/internal/worker/common"
+)
+
+type mockDataServer struct {
+	apiv1.UnimplementedDataServiceServer
+}
+
+func TestBaseDestWorker_ListensOnPort(
+	t *testing.T,
+) {
+	lis, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	port := lis.Addr().(*net.TCPAddr).Port
+	lis.Close()
+
+	w := common.BaseDestinationWorker{
+		Logger: logr.Discard(),
+		Config: common.DestinationConfig{
+			ServerPort: fmt.Sprintf("%d", port),
+		},
+	}
+
+	ctx, cancel := context.WithCancel(
+		context.Background(),
+	)
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- w.Run(ctx, &mockDataServer{})
+	}()
+
+	cancel()
+
+	if err := <-errCh; err != nil &&
+		err != context.Canceled {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
